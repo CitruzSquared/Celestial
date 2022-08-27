2b5e6e93b9e9f94844a76e75f8e8e9762afb1897
CitruzSquared <nckim73@gmail.com>
2022-08-27 16:28:38 +0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,13 +4,28 @@ function setup() {
 }
 let angle = 0;
 let starList;
+let celestialRadius = 200;
 function preload() {
     starList = loadStrings("starList.txt");
 }
 function draw() {
     background(0);
-    let v = new p5.Vector(1, 1, 1);
-    rotate(angle, v);
-    rect(0, 0, 100, 100);
-    angle++;
+    for (let i = 0; i < starList.length; i++) {
+        let values = split(starList[i]);
+        let star = new Star(values[0], values[1], values[2]);
+    }
+}
+
+class Star {
+    constructor(rightAscension, declination, magnitude) {
+        this.rightAscension = rightAscension;
+        this.declination = declination;
+        this.magnitude = magnitude;
+    }
+
+    drawStar() {
+        stroke(255);
+        strokeWidth(20 * sqrt(pow(pow(100, 0.2), -this.magnitude)));
+        point(celestialRadius * cos(this.rightAscension) * cos(this.declination), celestialRadius * sin(this.declination), -celestialRadius * cos(this.rightAscension) * cos(this.declination));
+    }
 }
