220a9e28284d74c81fbb5e385b8640d3366be8ec
CitruzSquared <nckim73@gmail.com>
2022-08-27 23:08:19 +0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -172,7 +172,7 @@ function setup() {
 
     sizelabel = createElement("h5", "Size");
     sizelabel.position(windowWidth - 235, 490);
-    sizeslider = createSlider(10, 70, 40, 6);
+    sizeslider = createSlider(10, 70, 30, 6);
     sizeslider.position(windowWidth - 185, 510);
 }
 
@@ -198,7 +198,7 @@ let celestialRadius = 1000;
 let starList = [];
 let latitude = 35;
 let tilt = 23;
-let size = 40;
+let size = 30;
 
 let siderealTime;
 
