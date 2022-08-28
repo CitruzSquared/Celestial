0894bef0c4f735014ab8339dd6450a6e5ee4c7d3
CitruzSquared <nckim73@gmail.com>
2022-08-28 13:29:58 +0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -221,6 +221,7 @@ let R = 126.096;
 let lunarObliquity = 4.85;
 let moonPeriod = 22.21118012;
 let moonoffset = 69.37267080852348993;
+let moonSynodic = 1 / (1 / moonPeriod - 1 / year);
 let planetA = new innerPlanet(169.0587391, 86.4136, 13.15, 0, 50, 100, 35);
 let planetC = new outerPlanet(543.7880553, 188.297, -60.31, 90, 50, 100, 30);
 let planetD = new outerPlanet(2934.3, 579.286, -1366.08, 200, 50, 100, 25);
@@ -371,7 +372,7 @@ function sv(t) {
     return 360 / year * (t - 0.5);
 }
 function calculateMoonPosition(t) {
-    let n = t % 24;
+    let n = t % moonSynodic;
     let s = 360 / moonPeriod * (n + moonoffset);
     let m = (s + 180) % 360 - 180;
     return (m - sunEclipticPosition(n) + 180) % 360 - 180;
