16f2aa1f5cdd09aa79013069f0dcf841fe0a3455
CitruzSquared <nckim73@gmail.com>
2022-09-07 12:29:10 +0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -119,7 +119,7 @@ function setup() {
 
     speedlabel = createElement("h5", "Speed");
     speedlabel.position(windowWidth - 235, 60);
-    speedslider = createSlider(-11, 11, 2, 1);
+    speedslider = createSlider(-12, 12, 2, 1);
     speedslider.position(windowWidth - 185, 80);
     speednumber = createElement("h5", "Speed: 1 spf");
     speednumber.position(windowWidth - 235, 85);
@@ -245,6 +245,14 @@ function draw() {
     else if (speedslider.value() < 0) {
         speed = -1 / 86400 * pow(2, -speedslider.value() - 2);
         speednumber.html("Speed: " + -1 * pow(2, -speedslider.value() - 2) + " spf");
+    }
+    if (speedslider.value === 12) {
+        speed = 86000;
+        speednumber.html("Speed: 1 dpf");
+    }
+    else if (speedslider.value === -12) {
+        speed = -86000;
+        speednumber.html("Speed: -1 dpf");
     }
     latnum.html("Latitude: " + latitude + "˚");
     tiltnum.html("Axial Tilt: " + tilt + "˚");
